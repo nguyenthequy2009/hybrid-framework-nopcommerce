2ab59732cfffacf7967e87c4fa8aee7f81571691
thequy <nguyenthequy2009@gmail.com>
2023-07-12 22:44:32 +0700

    init automation framework

---
+++ actions/commons/BasePage.java
@@ -0,0 +1,6 @@
+package commons;
+
+//Chứa các hàm dùng chung cho Các [pageObjects] của 1 application
+public class BasePage {
+
+}
---
+++ actions/commons/BaseTest.java
@@ -0,0 +1,6 @@
+package commons;
+
+// Chứa các hàm dùng chung cho tầng [testcases]
+public class BaseTest {
+
+}
