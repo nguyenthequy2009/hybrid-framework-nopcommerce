fae11d30f5c47c7ad84413fb16d7dc52c6e1b404
thequy <nguyenthequy2009@gmail.com>
2023-07-17 22:32:01 +0700

    Class BasePage

--- testcases/com/nopcommerce/user/Level_01_Register_Repeat_Yourself.java
+++ testcases/com/nopcommerce/user/Level_01_Register_Repeat_Yourself.java
@@ -11,7 +11,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class User_01_Register {
+public class Level_01_Register_Repeat_Yourself {
 	WebDriver driver;
 	String emailAddress;
 	String projectPath = System.getProperty("user.dir");
